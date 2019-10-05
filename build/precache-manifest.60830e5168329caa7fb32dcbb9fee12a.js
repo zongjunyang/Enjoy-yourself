9d1538adaec5c7de32bfa17c5d5f5c1049002725
zyang <zyang@cobo.com>
2019-10-05 10:37:45 +0800

    add build

---
+++ build/precache-manifest.60830e5168329caa7fb32dcbb9fee12a.js
@@ -0,0 +1,126 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed034b245abe5c14beaff54a6083ac98",
+    "url": "./index.html"
+  },
+  {
+    "revision": "ec6a38d1d3857a9079a0",
+    "url": "./static/css/main.501b04bc.chunk.css"
+  },
+  {
+    "revision": "105a87ce227a358d7cbc",
+    "url": "./static/js/2.f3c95708.chunk.js"
+  },
+  {
+    "revision": "ec6a38d1d3857a9079a0",
+    "url": "./static/js/main.8e196524.chunk.js"
+  },
+  {
+    "revision": "e7e7d897dbd6c57f9040",
+    "url": "./static/js/runtime-main.44d4c2bb.js"
+  },
+  {
+    "revision": "5845b9a586bc0f48f51a94a992f718a6",
+    "url": "./static/media/IMG_20160306_143216_HDR.5845b9a5.jpg"
+  },
+  {
+    "revision": "25e42f100deea78cb1ef05d3d686b7fe",
+    "url": "./static/media/IMG_20171231_161529.25e42f10.jpg"
+  },
+  {
+    "revision": "51b8114d5fc1ec3861ebae77c8c6347a",
+    "url": "./static/media/IMG_20171231_163538.51b8114d.jpg"
+  },
+  {
+    "revision": "8f41518c3065f1a03fb0d3c1086bd8f0",
+    "url": "./static/media/IMG_20180401_175158.8f41518c.jpg"
+  },
+  {
+    "revision": "c4c6cf714277e489bac104ae2adac646",
+    "url": "./static/media/IMG_20180401_175411.c4c6cf71.jpg"
+  },
+  {
+    "revision": "fcf5a8817c589ace5c44de2db807a1f1",
+    "url": "./static/media/IMG_20180622_213821.fcf5a881.jpg"
+  },
+  {
+    "revision": "08a8406099eb9dc5f13dcfb8762f0575",
+    "url": "./static/media/IMG_20180623_090728.08a84060.jpg"
+  },
+  {
+    "revision": "1c0d19f16a80d148750df93726a6e926",
+    "url": "./static/media/IMG_20180624_103904.1c0d19f1.jpg"
+  },
+  {
+    "revision": "c1cd467da66c6c22bf017fbb0438961e",
+    "url": "./static/media/IMG_20181020_122557.c1cd467d.jpg"
+  },
+  {
+    "revision": "46dd53cd96be898481de2a1c211a1c7a",
+    "url": "./static/media/IMG_20190215_181836.46dd53cd.jpg"
+  },
+  {
+    "revision": "b5dcbacec6391819e6a4b364c98ea3e2",
+    "url": "./static/media/IMG_20190317_145112.b5dcbace.jpg"
+  },
+  {
+    "revision": "e278f07072c49e91e53498bf4f227c7f",
+    "url": "./static/media/IMG_20190417_223732.e278f070.jpg"
+  },
+  {
+    "revision": "8b443165e4a90c525c58e84d75811b4d",
+    "url": "./static/media/IMG_20190419_184028-min.8b443165.jpg"
+  },
+  {
+    "revision": "ba8017832b8cd0f82fbe1d8e54827768",
+    "url": "./static/media/IMG_20190419_184659.ba801783.jpg"
+  },
+  {
+    "revision": "3704b8831181b0ab73fadbf3fa475910",
+    "url": "./static/media/IMG_20190419_185131.3704b883.jpg"
+  },
+  {
+    "revision": "8bb2e5df84423431c79b118fdb594da6",
+    "url": "./static/media/IMG_20190711_170151-min.8bb2e5df.jpg"
+  },
+  {
+    "revision": "c1cc479b2c98dccac6a13e81aa0621a5",
+    "url": "./static/media/IMG_20190711_211315-min.c1cc479b.jpg"
+  },
+  {
+    "revision": "b23ca5b0a0ac2a47521f2b666e6a9d93",
+    "url": "./static/media/IMG_20190713_111458.b23ca5b0.jpg"
+  },
+  {
+    "revision": "2128f3d5b54c3ea299b76e7df298615c",
+    "url": "./static/media/IMG_20190713_111537.2128f3d5.jpg"
+  },
+  {
+    "revision": "c96340dec2b429e9578cb03c6c439d0f",
+    "url": "./static/media/IMG_20190720_200148.c96340de.jpg"
+  },
+  {
+    "revision": "e21c6a99fc3b2dec0026c3684a825ee2",
+    "url": "./static/media/IMG_20190824_201434.e21c6a99.jpg"
+  },
+  {
+    "revision": "d2dfd6de974d451278fd2c711f09391d",
+    "url": "./static/media/IMG_20190914_184809.d2dfd6de.jpg"
+  },
+  {
+    "revision": "3952905de0554959ada6766e1d72714b",
+    "url": "./static/media/IMG_20190930_201653.3952905d.jpg"
+  },
+  {
+    "revision": "c80edb9169830518fb45cbb4845c67a1",
+    "url": "./static/media/WechatIMG31.c80edb91.jpeg"
+  },
+  {
+    "revision": "fccf727658ab45453094097532fbfef4",
+    "url": "./static/media/mmexport1563160861091-min.fccf7276.jpg"
+  },
+  {
+    "revision": "b8afd8bf2ac5e8ee681183753fb6e5f9",
+    "url": "./static/media/mmexport1563160862646-min.b8afd8bf.jpg"
+  }
+]);
